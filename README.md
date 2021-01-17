6be1709d1479a21492f0e92d4720591ae4173b04
hyunjeong kim <hyunjeong.kim@biomind.ai>
2021-01-17 23:37:37 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python_automation
-python automation scripts for easier life
+
+python automation scripts for the easier life
+
+1. lingoda_file_distributor
+    - Lingoda (language learning website), provides pdf format materials to distribute to the file in order to level and chapter
+    - Usage: put this script in Downloads folder and run the script (python3 lingoda_file_distributor.py)
+      the default language set is French (since I am not sure other language pdf material format)
--- lingoda_file_distributor.py
+++ lingoda_file_distributor.py
@@ -4,16 +4,17 @@ import shutil
 
 LINGODA_FOLDER_NAME = 'Desktop/Lingoda'
 DOWNLOAD_DIR = os.path.dirname(os.path.abspath(__file__))
+# DOWNLOAD_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(DOWNLOAD_DIR)
+LANGUAGE = 'FR' # FOR french
 lingda_files = []
 lingoda_path =os.path.join(BASE_DIR, LINGODA_FOLDER_NAME)
-
 
 for item in os.listdir(DOWNLOAD_DIR):
     if not os.path.isfile(os.path.join(DOWNLOAD_DIR, item)):
         continue
     file_src = str(DOWNLOAD_DIR + item)
-    if not (file_src.endswith('pdf') and 'FR' in file_src):
+    if not (file_src.endswith('pdf') and LANGUAGE in file_src):
         continue
     lingda_files.append(item)
     
@@ -24,9 +25,10 @@ for file_name in lingda_files:
     if len(lgd_format) != 3:
         print('ERROR: invalid file name format!')
         continue
+    lv = lgd_format[0]
     chapter = lgd_format[1]
     source_dir = os.path.join(DOWNLOAD_DIR, file_name)
-    target_dir = os.path.join(lingoda_path, lgd_format[0], f'{chapter[0]}.{chapter[2]}')
+    target_dir = os.path.join(lingoda_path, lv, f'{chapter[0]}.{chapter[2]}')
     os.makedirs(target_dir, exist_ok=True)
     shutil.move(source_dir, target_dir)
 print('Done!')
